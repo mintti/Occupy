edcba8dd1a58e47efe132e09f43bab7160130aaf
MINJI JEONG <mintti97@gmail.com>
2018-12-23 21:17:46 +0900

    Plyer 카메라

---
+++ Assets/2. Scripts/SmoothFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+	public class SmoothFollow : MonoBehaviour
+	{
+
+		// The target we are following
+		[SerializeField]
+		private Transform target;
+
+
+        private Vector3 targetPos;
+        private Vector3 Pos;
+		void Start() {
+            Pos = transform.position;
+        }
+
+		// Update is called once per frame
+		void LateUpdate()
+		{
+            targetPos = target.position;
+            transform.position = targetPos + Pos;
+            
+		}
+	}
+}
