26313f5110aec30b39977597fb6fd524478ce5e0
MINJI JEONG <mintti97@gmail.com>
2018-12-23 21:20:31 +0900

    Enemy * 교재의 기존 적 이동관리. 게임에 적합하게 일부 수정

---
+++ Assets/2. Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+	public PlayerCtrl Player;
+	public enum State
+	{
+		PATROL,
+		TRACE,
+		ATTACK,
+		DIE
+	}
+
+	public State state = State.PATROL;
+	private Transform playerTr;
+	private Transform enemyTr;
+
+	private Animator animator;
+	public float attackDist = 5.0f;
+	public float traceDist = 10.0f;
+	public bool isDie = false;
+
+	private WaitForSeconds ws;
+
+	private MoveAgent moveAgent;
+
+	private readonly int hashMove = Animator.StringToHash("IsMove");
+	private readonly int hashSpeed = Animator.StringToHash("Speed");
+	void Awake()
+	{
+		var player = GameObject.FindGameObjectWithTag("Player");
+
+		if(player!=null)
+			playerTr = player.GetComponent<Transform>();
+
+		enemyTr = GetComponent<Transform>();
+		animator = GetComponent<Animator>();
+		moveAgent = GetComponent<MoveAgent>();
+
+		ws = new WaitForSeconds(0.3f);
+	}
+	void Update()
+	{
+		//Speed 파라미터에 이동속도를 전달
+		animator.SetFloat(hashSpeed, moveAgent.speed);
+	}
+	void OnEnable()
+	{
+		StartCoroutine(CheckState());
+		StartCoroutine(Action());
+	}
+
+	IEnumerator CheckState(){
+		while(!isDie){
+			if(state == State.DIE) yield break;
+			float dist = (playerTr.position - enemyTr.position).sqrMagnitude;
+
+			if(Player.ValidAttack){
+				if(dist <= attackDist)
+					state = State.ATTACK;
+				state = State.TRACE;
+			}
+			else{
+				state = State.PATROL;
+			}
+			yield return new WaitForSeconds(0.3f);
+		}
+	}
+
+	IEnumerator Action()
+	{
+		while(!isDie)
+		{
+			yield return ws;
+			switch(state)
+			{
+				case State.PATROL :
+					moveAgent.patrolling = true;
+					animator.SetBool(hashMove, true);
+					break;
+				case State.TRACE :
+					moveAgent.traceTarget = playerTr.position;
+					animator.SetBool(hashMove, true);
+					break;
+				case State.ATTACK:
+					moveAgent.Stop();
+					animator.SetBool(hashMove, false);
+					break;
+				case State.DIE:
+					moveAgent.Stop();
+					break;
+				
+			}
+		}
+	}
+
+	public int GetEnemyDie(){
+		if(state == State.TRACE || state == State.ATTACK)
+			return 1;
+		return 0;
+	}
+}
---
+++ Assets/2. Scripts/Enemy/MoveAgent.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveAgent : MonoBehaviour {
+
+	public List<Transform> wayPoints;
+	public int nextIdx;
+
+	private readonly float patrolSpeed = 2.0f;
+	private readonly float traceSpeed = 4.0f;
+
+	//회전할 때의 속도를 조절
+	private float damping = 1.0f;
+	private NavMeshAgent agent;
+	
+	private Transform enemyTr;
+	public float speed
+	{
+		get{ return agent.velocity.magnitude;}
+	}
+	void Start () {
+		enemyTr = GetComponent<Transform>();
+		agent = GetComponent<NavMeshAgent>();
+
+		agent.autoBraking= false;
+		agent.updateRotation = false;
+
+		agent.speed = patrolSpeed;
+
+		var group = GameObject.Find("WayPointGroup");
+		if(group != null)
+		{
+			group.GetComponentsInChildren<Transform>(wayPoints);
+			wayPoints.RemoveAt(0);
+		}
+		this.patrolling = true;
+	}
+	void Update(){
+		//적 캐릭터가 이동중일때만 회전
+		if(agent.isStopped == false)
+		{
+			Quaternion rot = Quaternion.LookRotation(agent.desiredVelocity);
+			enemyTr.rotation = Quaternion.Slerp(enemyTr.rotation, rot, Time.deltaTime * damping);
+		}
+		if(!_patrolling) return;
+
+		if(agent.velocity.sqrMagnitude>=0.2f * 0.2f
+		&& agent.remainingDistance <= 0.5f)
+		{
+			nextIdx = ++nextIdx % wayPoints.Count;
+
+			MoveWayPoint();
+		}
+	}
+	// Update is called once per frame
+	void MoveWayPoint()
+	{
+		if(agent.isPathStale) return;
+
+
+		agent.destination = wayPoints[nextIdx].position;
+		agent.isStopped = false;
+	}
+
+	private bool _patrolling;
+	public bool patrolling
+	{
+		get{ return _patrolling;}
+		set
+		{
+			_patrolling  = value;
+			if(_patrolling)
+			{
+				agent.speed = patrolSpeed;
+				//순찰 상태의 회전 계수
+				damping = 1.0f;
+				MoveWayPoint();
+			}
+		}
+	}
+
+	private Vector3 _traceTarget;
+	public Vector3 traceTarget
+	{
+		get{return _traceTarget;}
+		set{
+			_traceTarget = value;
+			agent.speed = traceSpeed;
+			//추적 상태의 회전 계수
+			damping = 7.0f;
+			TraceTarget(_traceTarget);
+		}
+	}
+
+	//주인공을 주적할 때 이동시키는 함수
+	void TraceTarget(Vector3 pos)
+	{
+		if(agent.isPathStale) return;
+		agent.destination = pos;
+		agent.isStopped = false;
+	}
+
+	//순찰 및 추적을 중지시키는 함수
+	public void Stop()
+	{
+		agent.isStopped = true;
+		agent.velocity = Vector3.zero;
+		_patrolling = false;
+	}
+}
