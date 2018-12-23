990b7367bf0d23f5fab9bf841b3aa474f91846bd
MINJI JEONG <mintti97@gmail.com>
2018-12-23 21:20:58 +0900

    Item * 아이템 회전효과, 아이템 생성위치 안내 텍스트 제어

---
+++ Assets/2. Scripts/Deco/ItemRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnEnable()
+	{
+		StartCoroutine(ItemMover());
+	}
+
+	IEnumerator ItemMover(){
+		Debug.Log("실행");
+		transform.Rotate(new Vector3(0, 0, 10));
+		while(transform.rotation.z % 180 != 0){
+			transform.Rotate(new Vector3(0, 0, 10));
+			yield return new WaitForSeconds(0.05f);
+		}
+		
+	}
+}
---
+++ Assets/2. Scripts/Deco/ItemTextMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ItemTextMover : MonoBehaviour {
+
+	public Text arrow;
+	float nextTime;
+	float timeRate;
+	void Start(){
+		timeRate = 0;
+		nextTime = 3;
+	}
+	void Update(){
+		if(timeRate > nextTime){
+			Destroy(this.gameObject);
+		}
+		timeRate += Time.deltaTime;
+		transform.position += new Vector3(0, 0.5f, 0);
+	}
+
+	public void TextSetting(string text){
+		arrow.text = text;
+	}
+}
