39a4fc961a41bc80185cb75393f0bb06aee69990
MINJI JEONG <mintti97@gmail.com>
2018-12-23 21:18:20 +0900

    보스공격패턴

---
+++ Assets/2. Scripts/Enemy/EnemyPattern.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPattern : MonoBehaviour {
+	public UIController UI;
+	public EnemyAI Enemy;
+	float pr;
+	public CircleBoltTime pattern1;
+	public CircleBolt pattern2;
+	public BossShot pattern3;
+
+	public ParticleSystem Die;
+
+
+	// Use this for initialization
+	void Start () {
+		Enemy = GetComponent<EnemyAI>();
+		
+		pr = UI.pr;
+		
+		pattern1 = this.GetComponent<CircleBoltTime>();
+		pattern2 = this.GetComponent<CircleBolt>();
+		pattern3 = this.GetComponent<BossShot>();
+
+		pattern1.enabled= false;
+		pattern2.enabled= false;
+		pattern3.enabled= false;
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		pr = UI.pr;
+
+		switch(Enemy.GetEnemyDie()){
+			case 0 :{
+				if(pr<20){
+					pattern1.enabled = true;
+				}
+				else if(pr <80){
+					pattern1.enabled = false;
+					pattern2.enabled = true;
+					}
+				else if(pr < 80){
+					pattern2.enabled = false;
+					pattern3.enabled = true;
+				}
+				else{
+					Die.Play();
+				}
+				
+				break;
+			}
+			case 1 : {
+				pattern1.enabled= false;
+				pattern2.enabled= false;
+				pattern3.enabled= false;
+				break;
+			}
+
+		}
+	}
+}
