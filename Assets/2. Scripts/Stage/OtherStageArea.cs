0b98b5bba79f35b8e2eae6d4ebbb93a380405278
MINJI JEONG <mintti97@gmail.com>
2018-12-23 21:21:48 +0900

    Enemy * 적 이동 스팟들이 범위 외일 경우 Enable 제어

---
+++ Assets/2. Scripts/Stage/OtherStageArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherStageArea : MonoBehaviour {
+	public GameController Game;
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			Game = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (Game == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+	//적 이동 스팟들이 범위 외 일경우 상태 false해줌. WayPointGroup에 존재함.
+	public void ActiveOffWayPoint(){
+		for(int i = 0; i< transform.childCount ; i++){
+			Vector3 WayPointPos = transform.GetChild(i).transform.position;
+			if(WayPointPos.x < -Game.w/4 || WayPointPos.x > Game.w/4 ||
+				WayPointPos.z < -Game.h/4 || WayPointPos.z > Game.h/4 ){
+				transform.GetChild(i).gameObject.SetActive(false);
+			}
+		}
+	}
+}
